3ced8bec925a726ed364e103a8464cf4b25d5e07
Dave Frame <dframe22@gmail.com>
2020-04-23 16:45:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,8 @@ end
 
 def get_japanese_emoticon(file, emoticon)
   library = load_library(file)
-  binding.pry
   library.each_pair do |(word, symbols)|
+    binding.pry
     if symbols.include?(emoticon) == false
       p "Sorry, I don't know that one."
     end
